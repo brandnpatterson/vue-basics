9e98b6355cdf7b5dcb128e98dbd78a2417f8bb67
Brandon Patterson <brandnpatterson@gmail.com>
2017-09-02 10:48:42 -0500

    computed props, dynamic CSS, conditionals

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ new Vue({
     job: 'Front End Web Developer',
     website: 'http://www.brandnpatterson.com',
     websiteTag: '<a href="http://www.brandnpatterson.com">My Portfolio</a>',
+    a: 0,
+    b: 0,
     x: 0,
-    y: 0
+    y: 0,
+    available: false,
+    nearby: false,
+    error: false,
+    success: false
   },
   methods: {
     greet: function (time) {
@@ -23,6 +29,27 @@ new Vue({
     updateXY: function (e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
+    },
+    altLink: function () {
+      alert('You clicked me!');
+    },
+    log: function (event, element) {
+      console.log(event)
+      element = event.target.value;
+    }
+  },
+  computed: {
+    addToA: function () {
+      return this.a + this.age;
+    },
+    addToB: function () {
+      return this.b + this.age;
+    },
+    compClasses: function () {
+      return {
+        available: this.available,
+        nearby: this.nearby
+      }
     }
   }
 });
