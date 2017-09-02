ae964b20136a2336b954e3b57f95da9a8c487237
Brandon Patterson <brandnpatterson@gmail.com>
2017-09-02 09:17:55 -0500

    event binding and updating the dom

--- app.js
+++ app.js
@@ -6,11 +6,23 @@ new Vue({
     age: 29,
     job: 'Front End Web Developer',
     website: 'http://www.brandnpatterson.com',
-    websiteTag: '<a href="http://www.brandnpatterson.com">My Portfolio</a>'
+    websiteTag: '<a href="http://www.brandnpatterson.com">My Portfolio</a>',
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function (time) {
       return 'Good ' + time + ' ' + this.name + '!';
+    },
+    add: function (inc) {
+      return this.age += inc;
+    },
+    subtract: function (dec) {
+     return this.age -= dec;
+    },
+    updateXY: function (e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     }
   }
 });
