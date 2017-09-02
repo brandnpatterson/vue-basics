5880ac1b7dceb803a33ed4f10f2110ded6fff3b7
Brandon Patterson <brandnpatterson@gmail.com>
2017-09-02 10:51:39 -0500

    working with for

--- app.js
+++ app.js
@@ -2,27 +2,17 @@
 new Vue({
   el: '#app',
   data: {
-    name: 'Brandon',
-    age: 29,
-    job: 'Front End Web Developer',
-    website: 'http://www.brandnpatterson.com',
-    websiteTag: '<a href="http://www.brandnpatterson.com">My Portfolio</a>',
-    x: 0,
-    y: 0
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+    ninjas: [
+      { name: 'Ryu', age: 25 },
+      { name: 'Shen', age: 26 },
+      { name: 'Akali', age: 18 }
+    ]
   },
   methods: {
-    greet: function (time) {
-      return 'Good ' + time + ' ' + this.name + '!';
-    },
-    add: function (inc) {
-      return this.age += inc;
-    },
-    subtract: function (dec) {
-     return this.age -= dec;
-    },
-    updateXY: function (e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
-    }
+
+  },
+  computed: {
+    
   }
 });
