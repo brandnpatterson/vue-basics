4065921632bf53494f7264a798a45b54cff6a176
Brandon Patterson <brandnpatterson@gmail.com>
2017-09-02 11:53:01 -0500

    intro to components

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-// vue instance
+Vue.component('greeting', {
+  template: '<p>Hey there, I am a {{ name }}. <button v-on:click="changeName">Change Name</button></p>',
+  data: function () {
+    return {
+      name: 'Yoshi'
+    }
+  },
+  methods: {
+    changeName: function () {
+      this.name = 'Mario'
+    }
+  }
+});
+
 var one = new Vue({
   el: '#app-one',
   data: {
-    title: 'Vue App One'
+
   },
   methods: {
 
   },
   computed: {
-    greet: function () {
-      return 'Hello from app one';
-    }
+
   }
 });
 
 var two = new Vue({
   el: '#app-two',
   data: {
-    title: 'Vue App Two'
+
   },
   methods: {
-    changeTitle: function () {
-      one.title === 'Vue App One' ? one.title = 'Vue App New' : one.title = 'Vue App One';
-    }
+
   },
   computed: {
-    greet: function () {
-      return 'Hello from app two';
-    }
+
   }
 });
